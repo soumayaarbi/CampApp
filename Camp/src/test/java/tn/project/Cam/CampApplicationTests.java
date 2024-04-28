e0f09a9e5e77809a967a1d18346a5398949d0daf
SoumayaArbi <soumayaarbi997@gmail.com>
2024-04-28 21:54:36 +0200

    Coté Backend

---
+++ Camp/src/test/java/tn/project/Cam/CampApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.project.Cam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
