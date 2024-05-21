6ec7f50a1a534ddac47841ca0ae59aa038049003
soumaya <soumayaarbi997@gmail.com>
2024-05-21 16:03:02 +0200

    some changes

--- frontCamp/src/app/app-routing.module.ts
+++ frontCamp/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { AddReservationComponent } from './add-reservation/add-reservation.component';
 
 const routes: Routes = [
   { path: 'reservation', component: ReservationComponent },
   { path: '', component: MainComponent },
+  { path: 'add-reservation', component: AddReservationComponent },
+  { path: '', redirectTo: '/reservations', pathMatch: 'full' },
 ];
 
 @NgModule({
--- frontCamp/src/app/reservation/reservation.component.ts
+++ frontCamp/src/app/reservation/reservation.component.ts
@@ -1,7 +1,7 @@
 import { ReservationService } from './reservation.service';
 import { Reservation } from '../Models/reservation';
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog'; // Importer le service MatDialog
+
 import { AddReservationComponent } from '../add-reservation/add-reservation.component';
 
 @Component({
@@ -14,7 +14,7 @@ export class ReservationComponent implements OnInit {
 
   constructor(
     private reservationService: ReservationService,
-    private dialog: MatDialog // Injecter le service MatDialog
+    
   ) {}
 
   ngOnInit() {
@@ -29,17 +29,5 @@ export class ReservationComponent implements OnInit {
     );
   }
 
-  addReservation() {
-    // Ouvrir le dialogue d'ajout de réservation
-    const dialogRef = this.dialog.open(AddReservationComponent, {
-      width: '400px', // Définir la largeur du dialogue
-      disableClose: true, // Empêcher la fermeture du dialogue en cliquant en dehors de celui-ci
-    });
-
-    // Gérer les résultats du dialogue
-    dialogRef.afterClosed().subscribe((result) => {
-      // Actualiser les réservations si nécessaire
-      // this.loadReservations();
-    });
-  }
+  
 }
